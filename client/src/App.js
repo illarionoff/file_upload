aacaeda1fdb37c6b7fd24270582c2acb7c15ae4b
Afanasiy <Afanasiy@Irinas-MacBook-Pro.local>
2019-04-11 09:05:15 +0300

    Functionality added ver 1.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>Hello</p>
-      </div>
-    );
-  }
-}
+import FileUpload from "./components/FileUpload";
+
+const App = () => {
+  return (
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+        <i className="fab fa-react">React File Upload</i>
+      </h4>
+      <FileUpload />
+    </div>
+  );
+};
 
 export default App;
